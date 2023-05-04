403d88a920c3a596a325e2ae075363154dc02aa8
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-05-04 20:20:28 +0300

    Add ability to add product to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,32 @@
-import './Cart.css';
+import "./Cart.css";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart-context";
 
 export function Cart() {
-    return (
-      <div className='Cart'>
+  const { cart, setCart } = useContext(CartContext);
 
-      </div>
-    );
-  }
-  
+  const RemoveFromCart = (productId) => {
+    const index = cart.findIndex((product) => product.id === productId);
+    if (index >= 0) {
+      const newCart = [...cart];
+      newCart.splice(index, 1);
+      setCart(newCart);
+    }
+  };
+
+  return (
+    <div className="Cart">
+      <h2>Cart {cart.length}</h2>
+      <ul>
+        {cart.map((product, i) => (
+          <li key={i}>
+            {product.name} - {product.price}
+            <button onClick={() => RemoveFromCart(product.id)}>
+              Remove from Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
